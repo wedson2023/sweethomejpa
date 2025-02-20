a30605fc12a50bbaf229dac64570d94435cb20b9
Wedson Santos <wedson_cross@hotmail.com>
2025-02-20 14:02:14 -0300

    Filtro funcionando

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const handlebars = require('express-handlebars');
 const cors = require('cors')
+const moment = require('moment')
 const bodyParser = require('body-parser')
 
 const app = express()
@@ -18,6 +19,7 @@ app.use(express.static('public'))
 app.use(cors())
 
 const mongoose = require('mongoose')
+const { Types } = mongoose;
 
 const { Schema } = mongoose;
 
@@ -46,7 +48,7 @@ app.get('/anuncios', async (req, res) => {
             { $match: {} },
             {
                 $project: {
-                    _id: 0,
+                    _id: 1,
                     nome: 1,
                     bairro: 1,
                     hospedes: 1,
@@ -62,7 +64,7 @@ app.get('/anuncios', async (req, res) => {
     }
 });
 
-app.post('/anuncios', async (req, res) => {
+app.post('/cadastrar-anuncios', async (req, res) => {
 
     const { nome, bairro, hospedes, comissao } = req.body;
 
@@ -82,12 +84,72 @@ schema = new Schema({
     preco: Number,
     check_in: Date,
     check_out: Date,
-    hospedes: Number
+    hospedes: Number,
+    dias: Number,
+    situacao: String,
+    limpeza: {
+        type: String,
+        default: 'Pendente'
+    }
 });
 
 const Reservas = mongoose.model('reservas', schema);
 
-app.get('/reservas', async (req, res) => {
+app.get('/api/reservas', async (req, res) => {
+
+    let query = {}
+
+    if (Object.values(req.query).length) {
+
+        query = {
+            $and: [
+                {
+                    check_in: {
+                        $gte: new Date(req.query.start), $lte: new Date(req.query.end)
+                    }
+                }
+            ]
+        }
+
+        if (req.query.acomodacao) query.$and.push({ acomodacao: req.query.acomodacao })
+        if (req.query.nome) query.$and.push({ nome: { $regex: req.query.nome, $options: 'i' } })
+    }
+
+    try {
+
+        const data = await Reservas.aggregate([
+            {
+                $match: query
+            },
+            {
+                $project: {
+                    _id: 1,
+                    nome: 1,
+                    telefone: 1,
+                    acomodacao: 1,
+                    preco: 1,
+                    check_in: { $dateToString: { format: "%d/%m %H:%M", date: "$check_in" } },
+                    check_out: { $dateToString: { format: "%d/%m %H:%M", date: "$check_out" } },
+                    hospedes: 1,
+                    dias: 1,
+                    situacao: 1,
+                    limpeza: 1,
+                }
+            }
+        ]);
+
+        res.json(data);
+
+    } catch (e) {
+        console.log(e);
+    }
+})
+
+app.get('/adm/reservas', async (req, res) => {
+
+    const { nome, acomodacao, check_in, check_out } = req.query;
+
+    console.log(nome, acomodacao, check_in, check_out);
 
     try {
 
@@ -95,16 +157,20 @@ app.get('/reservas', async (req, res) => {
             { $match: {} },
             {
                 $project: {
-                    _id: 0,
+                    _id: 1,
                     nome: 1,
                     telefone: 1,
                     acomodacao: 1,
                     preco: 1,
-                    check_in: { $dateToString: { format: "%Y-%m-%d %H:%M:%S", date: "$check_in" } },
-                    check_out: { $dateToString: { format: "%Y-%m-%d %H:%M:%S", date: "$check_out" } },
+                    check_in: { $dateToString: { format: "%d/%m %H:%M", date: "$check_in" } },
+                    check_out: { $dateToString: { format: "%d/%m %H:%M", date: "$check_out" } },
                     hospedes: 1,
+                    dias: 1,
+                    situacao: 1,
+                    limpeza: 1,
                 }
-            }
+            },
+            { $sort: { situacao: 1 } }
         ]);
 
         res.render('reservas', { data });
@@ -114,17 +180,165 @@ app.get('/reservas', async (req, res) => {
     }
 })
 
-app.post('/reservas', async (req, res) => {
+app.post('/cadastrar-reservas', async (req, res) => {
 
-    const { nome, telefone, acomodacao, preco, check_in, check_out, hospedes } = req.body;
+    let { nome, telefone, acomodacao, preco, check_in, check_out, hospedes } = req.body;
+
+    check_in = moment(check_in);
+    check_out = moment(check_out);
+
+    const dias = check_out.diff(check_in, 'days');
+
+    const now = moment();
+
+    const situacao = check_in.diff(now, 'days') + ' dia(s)';
 
     try {
-        await Reservas.create({ nome, telefone, acomodacao, preco, check_in, check_out, hospedes });
+        await Reservas.create({ nome, telefone, acomodacao, preco, check_in, check_out, hospedes, dias, situacao });
+
+        const data = await Reservas.aggregate([
+            { $match: {} },
+            {
+                $project: {
+                    _id: 1,
+                    nome: 1,
+                    telefone: 1,
+                    acomodacao: 1,
+                    preco: 1,
+                    check_in: { $dateToString: { format: "%d/%m %H:%M", date: "$check_in" } },
+                    check_out: { $dateToString: { format: "%d/%m %H:%M", date: "$check_out" } },
+                    hospedes: 1,
+                    dias: 1,
+                    situacao: 1,
+                    limpeza: 1,
+                }
+            },
+            { $sort: { situacao: 1 } }
+        ]);
+
+        res.json(data)
+
     } catch (e) {
         console.log(e);
     }
+})
 
-    res.json({ message: 'ok' })
+app.post('/reservas', async (req, res) => {
+
+    try {
+
+        const data = await Reservas.aggregate([
+            { $match: { situacao: { $ne: 'Estádia encerrada' } } },
+            {
+                $project: {
+                    _id: 1,
+                    nome: 1,
+                    telefone: 1,
+                    acomodacao: 1,
+                    preco: 1,
+                    check_in_formatado: { $dateToString: { format: "%d/%m %H:%M", date: "$check_in" } },
+                    check_out_formatado: { $dateToString: { format: "%d/%m %H:%M", date: "$check_out" } },
+                    check_in: 1,
+                    check_out: 1,
+                    hospedes: 1,
+                    dias: 1,
+                    situacao: 1,
+                    limpeza: 1,
+                }
+            }
+        ]);
+
+        for (let i in data) {
+
+            const check_in = moment(data[i].check_in).format('YYYY-MM-DD H:mm:ss');
+            const check_out = moment(data[i].check_out).format('YYYY-MM-DD H:mm:ss');
+            const now = moment().format('YYYY-MM-DD H:mm:ss');
+
+            let object;
+
+            if (check_out <= now) object = { situacao: 'Estádia encerrada' };
+            else if (check_in <= now) object = { situacao: 'Estádia em andamento' };
+            else {
+                object = { situacao: moment(data[i].check_in).diff(moment(), 'days') + ' dias' };
+            }
+
+            await Reservas.updateOne({ _id: data[i]._id }, object)
+
+        }
+
+        res.json({ data })
+
+    } catch (e) {
+        console.log(e);
+    }
+})
+
+app.delete('/reservas/:id', async (req, res) => {
+    try {
+
+        await Reservas.deleteOne(({ _id: new Types.ObjectId(req.params.id) }))
+
+        const data = await Reservas.aggregate([
+            { $match: {} },
+            {
+                $project: {
+                    _id: 1,
+                    nome: 1,
+                    telefone: 1,
+                    acomodacao: 1,
+                    preco: 1,
+                    check_in: { $dateToString: { format: "%d/%m %H:%M", date: "$check_in" } },
+                    check_out: { $dateToString: { format: "%d/%m %H:%M", date: "$check_out" } },
+                    hospedes: 1,
+                    dias: 1,
+                    situacao: 1,
+                    limpeza: 1,
+                }
+            },
+            { $sort: { situacao: 1 } }
+        ]);
+
+        res.json(data)
+
+    } catch (e) {
+        console.log(e);
+    }
+})
+
+
+app.put('/situacao-limpeza/:id', async (req, res) => {
+
+    try {
+
+        const limpeza = req.body.limpeza === 'Feita' ? 'Pendente' : 'Feita';
+
+        await Reservas.updateOne({ _id: new Types.ObjectId(req.params.id) }, { limpeza })
+
+        const data = await Reservas.aggregate([
+            { $match: {} },
+            {
+                $project: {
+                    _id: 1,
+                    nome: 1,
+                    telefone: 1,
+                    acomodacao: 1,
+                    preco: 1,
+                    check_in: { $dateToString: { format: "%d/%m %H:%M", date: "$check_in" } },
+                    check_out: { $dateToString: { format: "%d/%m %H:%M", date: "$check_out" } },
+                    hospedes: 1,
+                    dias: 1,
+                    situacao: 1,
+                    limpeza: 1,
+                }
+            },
+            { $sort: { situacao: 1 } }
+        ]);
+
+        res.json(data)
+
+    } catch (e) {
+        console.log(e);
+    }
 })
 
 app.listen(port, () => {
--- public/js/main.js
+++ public/js/main.js
@@ -33,13 +33,17 @@ function fn_open_modal() {
         top: ${innerWidth <= 900 ? 0 : '50%'};
         margin-top: -${innerWidth <= 900 ? 0 : (modal.offsetHeight / 2)}px;
     `
-    
+
     const cadastro_reservas = document.querySelector('.cadastro-reservas');
-    const cadastro_anuncios = document.querySelector('.cadastro-anuncios');    
+    const cadastro_anuncios = document.querySelector('.cadastro-anuncios');
 
     cadastro_reservas?.addEventListener('submit', fn_cadastro_reservas);
     cadastro_anuncios?.addEventListener('submit', fn_cadastro_anuncios);
 }
+
+const pesquisar_reservas = document.querySelector('.pesquisar-reservas');
+
+pesquisar_reservas.addEventListener('submit', fn_pesquisar_reservas);
 
 function fn_close_modal() {
     screen.style.opacity = 0;
@@ -60,7 +64,76 @@ screen.addEventListener('click', () => {
     fn_close_modal();
 })
 
-function fn_cadastro_reservas(e) {
+let loading = {};
+
+document.querySelectorAll('td.limpeza').forEach(e => {
+
+    const _id = e.getAttribute('id');
+
+    loading[_id] = false;
+
+    e.addEventListener('click', () => {
+        if (!loading[_id]) {
+            loading[_id] = true
+            fn_situacao_limpeza(e, _id);
+        }
+    })
+});
+
+document.querySelectorAll('td span.reservas').forEach(e => {
+
+    e.addEventListener('click', () => {
+
+        const _id = e.getAttribute('id');
+
+        loading[_id] = false;
+
+        if (!loading[_id]) {
+
+            const con = confirm('Tem certeza que deseja deletar esse registro?');
+
+            if (con) {
+                loading[_id] = true
+                fn_remover_reservas(_id);
+                loading[_id] = false
+
+            }
+
+        }
+    })
+});
+
+async function fn_pesquisar_reservas(e) {
+
+    e.preventDefault();
+
+    let data = {};
+
+    for (let i = 0; i < e.target.elements.length; i++) {
+        let name = e.target.elements[i].name;
+        let value = e.target.elements[i].value;
+
+        if (value)
+            data[name] = value;
+    }
+
+    const query = new URLSearchParams(data).toString();
+
+    try {
+
+        data = await fetch(`http://localhost:3000/api/reservas?${query}`)
+
+        data = await data.json();
+
+        fn_registros_reservas(data);
+
+    } catch (e) {
+        alert(e);
+    }
+
+}
+
+async function fn_cadastro_reservas(e) {
 
     e.preventDefault();
 
@@ -72,20 +145,25 @@ function fn_cadastro_reservas(e) {
 
         if (name)
             data[name] = value;
-
     }
 
-    fetch('http://localhost:3000/reservas', {
-        method: 'POST',
-        headers: { 'Content-type': 'application/json;charset=UTF-8' },
-        body: JSON.stringify(data)
-    })
-        .then(response => response.json())
-        .then(json => {
-            console.log(json)
-            fn_close_modal();
+    try {
+
+        data = await fetch('http://localhost:3000/cadastrar-reservas', {
+            method: 'POST',
+            headers: { 'Content-type': 'application/json;charset=UTF-8' },
+            body: JSON.stringify(data)
         })
-        .catch(err => console.log(err));
+
+        data = await data.json();
+
+        fn_registros_reservas(data);
+
+        fn_close_modal();
+
+    } catch (e) {
+        alert(e);
+    }
 }
 
 function fn_cadastro_anuncios(e) {
@@ -115,6 +193,178 @@ function fn_cadastro_anuncios(e) {
         })
         .catch(err => console.log(err));
 }
+
+async function fn_remover_reservas(_id) {
+
+    try {
+
+        let data = await fetch(`http://localhost:3000/reservas/${_id}`, { method: 'DELETE' })
+        data = await data.json();
+
+        fn_registros_reservas(data);
+
+        fn_close_modal();
+
+    } catch (e) {
+        alert(e);
+    }
+}
+
+
+// function fn_remover_anuncios(e, _id) {
+
+//     fetch(`http://localhost:3000/anuncios/${_id}`, {
+//         method: 'DELETE',
+//         headers: { 'Content-type': 'application/json;charset=UTF-8' }
+//     })
+//         .then(response => response.json())
+//         .then(json => {
+//             console.log(json)
+//             fn_close_modal();
+//         })
+//         .catch(err => console.log(err));
+// }
+
+async function fn_situacao_limpeza(e, _id) {
+
+    try {
+
+        const limpeza = e.getAttribute('situacao');
+
+        let data = await fetch(`http://localhost:3000/situacao-limpeza/${_id}`, {
+            method: 'PUT',
+            headers: { 'Content-type': 'application/json;charset=UTF-8' },
+            body: JSON.stringify({ limpeza })
+        })
+
+        data = await data.json();
+
+        fn_registros_reservas(data);
+
+        fn_close_modal();
+
+    } catch (e) {
+        alert(e);
+    }
+}
+
+function fn_registros_reservas(data) {
+
+    let tbody = document.querySelector('tbody');
+    tbody.innerHTML = "";
+
+    let tr, td, a;
+
+    for (let i in data) {
+
+        tr = document.createElement('tr');
+        td = document.createElement('td');
+        a = document.createElement('a');
+
+        let span = document.createElement('span');
+        span.innerText = 'delete';
+        span.setAttribute('class', 'material-icons');
+        span.classList.add('reservas');
+        span.setAttribute('id', data[i]._id);
+
+        td.appendChild(span);
+        tr.appendChild(td);
+
+        td = document.createElement('td');
+        td.innerText = data[i].nome;
+        tr.appendChild(td);
+
+        td = document.createElement('td');
+        a.setAttribute('href', `https://wa.me/55${data[i].telefone}`)
+        a.setAttribute('target', `_blank`)
+        a.setAttribute('title', `Clique para acessar o whats app do hospede.`)
+        a.innerText = data[i].telefone;
+
+        td.appendChild(a);
+        tr.appendChild(td);
+
+        td = document.createElement('td');
+        td.innerText = data[i].acomodacao;
+        tr.appendChild(td);
+
+        td = document.createElement('td');
+        td.innerText = data[i].preco;
+        tr.appendChild(td);
+
+        td = document.createElement('td');
+        td.innerText = data[i].check_in;
+        tr.appendChild(td);
+
+        td = document.createElement('td');
+        td.innerText = data[i].check_out;
+        tr.appendChild(td);
+
+        td = document.createElement('td');
+        td.innerText = data[i].hospedes;
+        tr.appendChild(td);
+
+        td = document.createElement('td');
+        td.innerText = data[i].dias;
+        tr.appendChild(td);
+
+        td = document.createElement('td');
+        td.innerText = data[i].situacao;
+        td.setAttribute('class', data[i].situacao);
+        tr.appendChild(td);
+
+        td = document.createElement('td');
+        td.innerText = data[i].limpeza;
+        td.setAttribute('id', data[i]._id);
+        td.setAttribute('class', 'limpeza');
+        td.setAttribute('situacao', data[i].limpeza);
+        td.classList.add(data[i].limpeza);
+        tr.appendChild(td);
+
+        tbody.appendChild(tr);
+
+    }
+
+    document.querySelectorAll('td span.reservas').forEach(e => {
+
+        e.addEventListener('click', () => {
+
+            const _id = e.getAttribute('id');
+
+            loading[_id] = false;
+
+            if (!loading[_id]) {
+
+                const con = confirm('Tem certeza que deseja deletar esse registro?');
+
+                if (con) {
+                    loading[_id] = true
+                    fn_remover_reservas(_id);
+                    loading[_id] = false
+
+                }
+
+            }
+        })
+    });
+
+    document.querySelectorAll('td.limpeza').forEach(e => {
+
+        const _id = e.getAttribute('id');
+
+        loading[_id] = false;
+
+        e.addEventListener('click', () => {
+            if (!loading[_id]) {
+                console.log('entrou');
+                loading[_id] = true
+                fn_situacao_limpeza(e, _id);
+                loading[_id] = false
+            }
+        })
+    });
+
+}
+
 
 
 
